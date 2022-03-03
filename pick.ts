94016694e236e4d47a01145a9e96847b3e6fe753
JasonLyy <7962578+JasonLyy@users.noreply.github.com>
2022-03-03 22:14:49 +1100

    add pick

---
+++ pick.ts
@@ -0,0 +1,17 @@
+//github.com/type-challenges/type-challenges/blob/master/questions/4-easy-pick/README.md
+https: type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key];
+};
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = MyPick<Todo, "title" | "completed">;
+
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+};
